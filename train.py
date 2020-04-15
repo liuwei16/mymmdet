7f5e93b0c6d3a76ab8563bacb8dca9083a99ef80
liuwei16 <liuwei16@nudt.edu.cn>
2020-04-15 10:30:53 +0800

    add train

---
+++ train.py
@@ -0,0 +1,39 @@
+from __future__ import division
+import argparse
+import copy
+import os
+import os.path as osp
+import glob
+import time
+import numpy as np
+import mmcv
+import torch
+from mmcv import Config
+from mmcv.runner import init_dist
+
+# from mmdet import __version__
+# from mmdet.apis import set_random_seed, train_detector
+from mmdet.datasets import build_dataset
+
+# path = 'data/insects/val/images/*.jpeg'
+# with open('data/insects/val.txt', 'w') as f:
+#     for i in glob.glob(path):
+#         f.write(osp.splitext(osp.basename(i))[0]+'\n')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='mydet')
+    parser.add_argument('--config', default='config_example.py', help='train config file path')
+
+def main():
+    args = parse_args()
+    cfg = Config.fromfile(args.config)
+    dataset = build_dataset(cfg.data.train)
+    for i in range(len(dataset)):
+        data = dataset[i]
+
+
+
+
+if __name__ == '__main__':
+    main()
